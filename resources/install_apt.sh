60d4e45b2e199726d8a22410d9ea466ee11ca7f4
Aegis940 <sylvain.villien@gmail.com>
2020-12-28 10:42:31 +0100

    Update install_apt.sh

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -11,18 +11,18 @@ touch "${PROGRESS_FILE}"
 
 echo 0 > "${PROGRESS_FILE}"
 echo "********************************************************"
-echo "*             Installation des dépendances             *"
+echo "             Installation des dépendances               "
 echo "********************************************************"
 
 echo 5 > "${PROGRESS_FILE}"
 echo "********************************************************"
-echo "*        Update package lists from repositories        *"
+echo "        Update package lists from repositories          "
 echo "********************************************************"
 sudo apt-get update
  
 echo 20 > "${PROGRESS_FILE}"
 echo "********************************************************"
-echo "*         Install Firefox                              *"
+echo "                     Install Firefox                    "
 echo "********************************************************"
 sudo apt install -y firefox-esr
 
@@ -34,13 +34,13 @@ fi
  
 echo 30 > "${PROGRESS_FILE}"
 echo "********************************************************"
-echo "*         Install iceweasel and xvfb                   *"
+echo "              Install iceweasel and xvfb                "
 echo "********************************************************"
 sudo apt-get install -y xvfb iceweasel
 
 echo 40 > "${PROGRESS_FILE}"
 echo "********************************************************"
-echo "*         Install geckodriver driver                   *"
+echo "              Install geckodriver driver                "
 echo "********************************************************"
 
 driver_version=""
@@ -63,14 +63,10 @@ if [ $( uname -s ) == "Linux" ]; then
 		driver_name="geckodriver-$driver_version-linux32.tar.gz";;
 	*)
 		echo "other : $(uname -m)"
-		echo "not supported"
-		rm ${PROGRESS_FILE}
-		exit 1;;
+		echo "not supported";;
 	esac
 else
 	echo "$(uname -s) not supported"
-	rm ${PROGRESS_FILE}
-	exit 1
 fi
 
 if [ $driver_version != "" ]; then
@@ -78,58 +74,91 @@ if [ $driver_version != "" ]; then
 	sudo tar xzfz $driver_name
 	sudo mv geckodriver /usr/local/bin
 	sudo rm $driver_name
+
+	if [ -f "/usr/local/bin/geckodriver" ]; then
+		echo "geckodriver driver successfully installed"
+	else
+		echo "Error in geckodriver driver installation"
+	fi	
 fi
 
 echo 50 > "${PROGRESS_FILE}"
 echo "********************************************************"
-echo "*         Install Python3 and dependencies             *"
+echo "            Install Python3 and dependencies            "
 echo "********************************************************"
 sudo apt-get install -y python3 python3-pip
 
 # Check Python version
-if [ $($python --version 2>&1 | grep -c 'Python 3.7.') == "0" ]; then
-	echo "Python version must be 3.7.x for Veolia IDF WebSite"
-	echo "Current version is: $(python --version)"
+if [ $(python3 --version 2>&1 | grep -c 'Python 3.7.') == "0" ]; then
+	echo "Warning: Python version must be 3.7.x for Veolia IDF WebSite"
+	echo "Current version is: $(python3 --version)"
 fi
 
 echo 70 > "${PROGRESS_FILE}"
 echo "********************************************************"
-echo "*             Python3 'requests' module                *"
+echo "               Python3 'requests' module                "
 echo "********************************************************"
 sudo pip3 install requests
 
 echo 75 > "${PROGRESS_FILE}"
 echo "********************************************************"
-echo "*             Python3 'lxml' module                    *"
+echo "               Python3 'lxml' module                    "
 echo "********************************************************"
 sudo pip3 install lxml
 
 echo 80 > "${PROGRESS_FILE}"
 echo "********************************************************"
-echo "*             Python3 'xlrd' module                    *"
+echo "               Python3 'xlrd' module                    "
 echo "********************************************************"
 sudo pip3 install xlrd
 
 echo 85 > "${PROGRESS_FILE}"
 echo "********************************************************"
-echo "*              Python3 'selenium' module               *"
+echo "               Python3 'selenium' module                "
 echo "********************************************************"
 sudo pip3 install selenium
 
 echo 90 > "${PROGRESS_FILE}"
 echo "********************************************************"
-echo "*              Python3 'pyvirtualdisplay' module       *"
+echo "               Python3 'pyvirtualdisplay' module        "
 echo "********************************************************"
 sudo pip3 install pyvirtualdisplay
 
 echo 95 > "${PROGRESS_FILE}"
 echo "********************************************************"
-echo "*              Python3 'urllib3' module                *"
+echo "               Python3 'urllib3' module                 "
 echo "********************************************************"
 sudo pip3 install urllib3
 
 echo 100 > "${PROGRESS_FILE}"
 echo "********************************************************"
-echo "*             Installation terminée                    *"
+echo "              Installation terminée                     "
 echo "********************************************************"
+
+echo "Résumé installation :"
+echo
+
+echo "1. $(firefox --version)"
+if [ $(firefox --version 2>&1 | sed -e "s/.* \([0-9][0-9]*\)\..*/\1/") -lt 60 ]; then
+	echo "Warning: Mozilla Firefox must be 60.x or higher for Veolia IDF WebSite. The Python script will not be able to be executed."
+fi
+
+echo "2. geckodriver driver:"
+if [ -f "/usr/local/bin/geckodriver" ]; then
+	echo "geckodriver driver $driver_version for $(uname -m)"
+else
+	echo "Warning: geckodriver driver missing."
+fi
+
+echo "3. Packages:"
+echo "$(sudo  dpkg --get-selections | grep -v deinstall | grep -E "xvfb|firefox|iceweasel|python3\-pip|python3\-requests|python3\-urllib3")"
+
+echo "4. $(python3 --version)"
+if [ $(python3 --version 2>&1 | grep -c 'Python 3.7.') == "0" ]; then
+	echo "Warning: Python version must be 3.7.x for Veolia IDF WebSite. The Python script will not be able to be executed."
+fi
+
+echo "5. Python modules:"
+echo "$(sudo pip3 list --format=legacy | grep -E "requests|lxml|xlrd|selenium|PyVirtualDisplay|urllib3")"
+
 rm "${PROGRESS_FILE}"
