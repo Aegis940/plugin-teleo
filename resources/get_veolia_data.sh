5e2e0ccaa0d82b2337462acd26de49a8742fac3f
Aegis940 <76250825+Aegis940@users.noreply.github.com>
2023-10-08 10:21:13 +0200

    Somes corrections
    Somes corrections

--- resources/get_veolia_data.sh
+++ resources/get_veolia_data.sh
@@ -23,7 +23,7 @@ timeout=180
 
 # Check Python version
 if [ "$1" = "IDF" ]; then
-	if [ $($python --version 2>&1 | grep -Ec 3\.(7|9)\.) == "0" ]; then
+	if [ $($python --version 2>&1 | grep -Ec '3\.(7|9)\.') == "0" ]; then
 		echo "Python version must be 3.7.x or 3.9.x"
 		exit 0
 	fi
